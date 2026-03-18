e4af233ddf1d32ca18811e329c3ea6cbe0812201
Dan Fortunato <dan.fortunato@gmail.com>
2026-03-17 20:23:27 -0400

    Clean up cube2hari

--- cube2hari.m
+++ cube2hari.m
@@ -5,9 +5,7 @@ function [HfC, CfH] = cube2hari(ncheb, nside)
 % - Interpolate from Chebyshev type-1 grid to Hari's grid (Chebyshev type-2 grid
 %   with the endpoints dropped)
 
-HfC = zeros(6*ncheb^2*nside^2);
-CfH = zeros(6*ncheb^2*nside^2);
-
+idx = reorder(ncheb, nside);
 [x, ~, w] = chebpts(ncheb, 1);
 xhari = chebpts(ncheb+2, 2);
 xhari([1 end]) = [];
@@ -21,13 +19,6 @@ Pblock = repmat({PP}, 6*nside^2, 1);
 Qblock = repmat({QQ}, 6*nside^2, 1);
 PPP = matlab.internal.math.blkdiag(Pblock{:});
 QQQ = matlab.internal.math.blkdiag(Qblock{:});
-
-idx = reorder(ncheb, nside);
-% HfC(:,idx) = PPP;
-% CfH(idx,:) = QQQ;
-% HfC = sparse(HfC);
-% CfH = sparse(CfH);
-
 [pi, pj, pv] = find(PPP);
 [qi, qj, qv] = find(QQQ);
 n = 6*ncheb^2*nside^2;
